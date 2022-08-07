d00d0fc7abab5779715844343337e9c25a68fa40
Paulo <pauloh16.pdr@gmail.com>
2022-08-06 23:48:55 -0300

    Inicializar servidor com web com go

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var tmp = template.Must(template.ParseGlob("templates/*.html"))
+
+func index(w http.ResponseWriter, r *http.Request) {
+	tmp.ExecuteTemplate(w, "Index", nil)
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
